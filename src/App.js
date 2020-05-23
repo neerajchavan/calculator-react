1d85b156e8ee87f16c8105daa55673539ef3de63
NeerajChavan <chavan.n33raj@gmail.com>
2020-05-23 15:06:18 +0530

    Functionality Added, few bugs to solve

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      count:'0',
+      exception : ''
+    }
+
+  this.resetScreen = this.resetScreen.bind(this)
+  this.addDot = this.addDot.bind(this)
+  this.calculate = this.calculate.bind(this)
+  this.resetScreen = this.resetScreen.bind(this)
+  }
+
+
+  resetScreen()
+  {
+    this.setState({
+      count: '0'
+    })
+  }
+
+
+
+  addDot = val => {
+    // only add decimal if there is no current decimal point present in the input area
+    if (this.state.count.indexOf(".") === -1) {
+      this.setState({ count: this.state.count + val });
+    }
+  };
+
+
+  calculate(){
+    const {count}=this.state
+
+    try
+    {
+    this.setState({
+      count:eval(count)
+    })
+    }
+
+    catch(exeption)
+    {
+      console.log("Syntax errr!",exeption)
+    }
+  }
+
+  changeValue(digit)
+  {
+    const {count}=this.state
+
+    this.setState(
+      {
+       count : count=='0'? String(digit) : count + digit
+      })
+  }
+
+
+render(){
+    return (
+ <div className=''>
+
+    <div className=''>
+      <input value={this.state.count}/>
     </div>
-  );
+
+    <div className=''>
+      <button onClick={()=> this.changeValue(1)}>1</button>
+      <button onClick={()=> this.changeValue(2)}>2</button>
+      <button onClick={()=> this.changeValue(3)}>3</button>
+      <button onClick={()=> this.changeValue('+')}>+</button>
+    </div>
+
+    <div className=''>
+      <button onClick={()=> this.changeValue(4)}>4</button>
+      <button onClick={()=> this.changeValue(5)}>5</button>
+      <button onClick={()=> this.changeValue(6)}>6</button>
+      <button onClick={()=> this.changeValue('-')}>-</button>
+    </div>
+
+    <div className=''>
+      <button onClick={()=> this.changeValue(7)}>7</button>
+      <button onClick={()=> this.changeValue(8)}>8</button>
+      <button onClick={()=> this.changeValue(9)}>9</button>
+      <button onClick={()=> this.changeValue('*')}>*</button>
+    </div>
+
+    <div className=''>
+      <button onClick={()=> this.changeValue(0)}>0</button>
+      <button onClick={this.resetScreen}>C</button>
+      <button onClick={()=> this.addDot()}>.</button>
+      <button onClick={()=> this.changeValue('/')}>/</button>
+    </div>
+
+    <div className='row'>
+      <button className='calculate' onClick={()=>this.calculate()}>=</button>
+    </div>
+
+    <div>
+     <h1>{this.state.exception}</h1>
+    </div>
+
+ </div>
+  )
+}
 }
 
 export default App;
